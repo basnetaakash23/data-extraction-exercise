9ad08bc2661563b08293848b5770aaee4db7a174
basnetaakash23 <aakash.basnet@bison.howard.edu>
2025-05-13 22:33:14 -0500

    data extraction exercise using jdbc vs hibernate

---
+++ src/main/java/com/ecommerce/data_extraction/domain/Orders.java
@@ -0,0 +1,35 @@
+package com.ecommerce.data_extraction.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.relational.core.mapping.Column;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "orders_v2")
+@RequiredArgsConstructor
+@Data
+public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "order_seq_gen")
+    @SequenceGenerator(name = "order_seq_gen", sequenceName = "orders_v2_seq", allocationSize = 200)
+    private Long id;
+
+    private String invoiceNo;
+    private String stockCode;
+
+    private String description;
+
+    private int quantity;
+    private Timestamp invoiceDate;
+    private BigDecimal unitPrice;
+    private Long customerId;
+    private String country;
+
+    // Getters and setters omitted for brevity
+}
